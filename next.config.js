dee54231b33fdd2a5fcdaacbbb41099015bca2d7
Robert Gorbul <rgorbul@gmail.com>
2021-03-20 20:31:52 +0300

    Add next-compose-plugins

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+const withPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
+module.exports = withPlugins([[withBundleAnalyzer]], {
   reactStrictMode: true,
 });
